519a4f79f15f62b7f69e4d465d9e0786f1ef91fb
hedongjin <hedongjin@camera360.com>
2017-08-02 16:38:13 +0800

    [何东津] 添加发布jcenter功能

--- app/src/main/java/us/pinguo/messer/demo/MainActivity.kt
+++ app/src/main/java/us/pinguo/messer/demo/MainActivity.kt
@@ -10,16 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TinyPref.getInstance().putString("test1", "addasda")
-        TinyPref.getInstance().putInt("testInt", 23213)
-
-        val giftData = LiveGiftDataInfo()
-        giftData.id = "ad"
-        giftData.ctime = System.currentTimeMillis()
-        val giftInfoList = arrayListOf<LiveGiftInfo>()
-        val giftInfo = LiveGiftInfo()
-        giftInfo.guid = System.currentTimeMillis().toString()
-        giftInfoList.add(giftInfo)
-        BroadcastDataDbHelper.getInstance(applicationContext).insertGiftData(giftData, giftInfoList)
     }
 }
--- pgmesser/src/main/java/us/pinguo/messer/home/WindowCompat.kt
+++ pgmesser/src/main/java/us/pinguo/messer/home/WindowCompat.kt
@@ -7,45 +7,25 @@ import android.view.WindowManager
 /**
  * Created by hedongjin on 2017/6/26.
  */
-class WindowCompat {
+object WindowCompat {
 
-    // 伴生对象
-    companion object {
+    fun startWindow(context: Context, window: AbstractWindow) {
+        window.dispatchCreate()
 
-        fun startWindow(context: Context, window: AbstractWindow) {
-            window?.let {
-                window.dispatchCreate()
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.addView(window.getView(), window.getLayoutParams())
 
-                window.getView()?.let {
-                    val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-                    windowManager.addView(it, window.getLayoutParams())
-                }
-
-                window.dispatchResume()
-            }
+        window.dispatchResume()
 
 
-        }
-
-        fun stopWindow(context: Context, window: AbstractWindow) {
-            window.dispatchPause()
-
-            window.getView()?.let {
-                val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-                windowManager.removeView(it)
-            }
-
-            window.dispatchDestory()
-        }
-
-        fun updateViewLayout(context: Context, view: View, layoutParams: WindowManager.LayoutParams) {
-            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-            windowManager.updateViewLayout(view, layoutParams)
-        }
-
-        fun String.hh() {
-
-        }
     }
 
+    fun stopWindow(context: Context, window: AbstractWindow) {
+        window.dispatchPause()
+
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.removeView(window.getView())
+
+        window.dispatchDestory()
+    }
 }
