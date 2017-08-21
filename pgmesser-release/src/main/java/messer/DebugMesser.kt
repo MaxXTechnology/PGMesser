3503da2549c2fd69d0a46bfcc55f0bfeb932f66b
hedongjin <hedongjin@camera360.com>
2017-08-21 13:55:32 +0800

    [何东津] 添加分布内网逻辑

---
+++ pgmesser-release/src/main/java/messer/DebugMesser.kt
@@ -0,0 +1,12 @@
+package us.pinguo.messer
+
+import android.app.Application
+
+/**
+ * Created by hedongjin on 2017/8/1.
+ */
+object DebugMesser {
+    fun install(context: Application) {
+
+    }
+}
