60ed6a1b35b89ec2e159ed15f370db1b6fc4e28f
hedongjin <hedongjin@camera360.com>
2017-08-23 11:17:47 +0800

    [何东津] 删除部分代码

--- pgmesser-debug/src/main/java/us/pinguo/messer/DebugMesser.kt
+++ pgmesser-debug/src/main/java/us/pinguo/messer/DebugMesser.kt
@@ -1,7 +1,14 @@
 package us.pinguo.messer
 
+import android.Manifest
 import android.app.Application
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator
 import com.nostra13.universalimageloader.core.DisplayImageOptions
 import com.nostra13.universalimageloader.core.ImageLoader
@@ -69,7 +76,25 @@ object DebugMesser {
             }
         })
 
-        MesserWindowManager.getInstance().gotoShortcut()
+        if (Build.VERSION.SDK_INT < 23 || ContextCompat.checkSelfPermission(context, Manifest.permission.SYSTEM_ALERT_WINDOW) == PackageManager.PERMISSION_GRANTED) {
+            MesserWindowManager.getInstance().gotoShortcut()
+        } else {
+            gotoSystemPermission(context)
+        }
+    }
+
+    fun gotoSystemPermission(context: Application) {
+        val localIntent = Intent()
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (Build.VERSION.SDK_INT >= 9) {
+            localIntent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
+            localIntent.data = Uri.fromParts("package", context.packageName, null)
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            localIntent.action = Intent.ACTION_VIEW;
+            localIntent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails")
+            localIntent.putExtra("com.android.settings.ApplicationPkgName", context.packageName)
+        }
+        context.startActivity(localIntent)
     }
 
 
